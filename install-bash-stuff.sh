f454a54db4412e0645901d0bfbacf53e78567843
Josh Trommel <josh@imjosh.me>
2014-03-28 19:45:45 -0700

    sd

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -LO https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile
-curl -LO https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc
