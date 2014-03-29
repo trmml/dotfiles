91c1ba5879b325d4d7ba9dcfdde61aebf91fe2fd
Josh Trommel <josh@imjosh.me>
2014-03-29 16:38:47 -0700

    i hope that works

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+currentdir=$(pwd)
+
 if [[ ! -d "/usr/local/bucket" ]]; then
   mkdir /usr/local/bucket
 fi
 
-curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile >/dev/null 2>&1
-curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc >/dev/null 2>&1
+cd ~
 
-currentdir=$(pwd)
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile >/dev/null 2>&1
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc >/dev/null 2>&1
+
 cd /usr/local/bucket
-
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh >/dev/null 2>&1
 mv update.sh update
 chmod +x /usr/local/bucket/update
