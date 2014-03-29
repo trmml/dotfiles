9bdb91d06935608176fc0ea950c0dce4fb4ddd06
Josh Trommel <joshception@icloud.com>
2014-03-29 14:55:49 -0700

    whoops

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -6,7 +6,7 @@ fi
 
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile >/dev/null 2>&1
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc >/dev/null 2>&1
-curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh /usr/local/bucket/update >/dev/null 2>&1
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh > /usr/local/bucket/update >/dev/null 2>&1
 chmod +x /usr/local/bucket/update
 
 echo "dotfiles have been updated"
