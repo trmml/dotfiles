d8aa2d9923e058c5a60fd401982036100303a5c2
Josh Trommel <joshception@icloud.com>
2014-03-28 21:11:27 -0700

    SILENT the shit out of curl

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -4,7 +4,7 @@ if [[ ! -d "/usr/local/bucket" ]]; then
   mkdir /usr/local/bucket
 fi
 
-curl -Os https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile
-curl -Os https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc
-curl -Os https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh /usr/local/bucket/update
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile ~/.bash_profile >/dev/null 2>&1
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc ~/.bashrc >/dev/null 2>&1
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh /usr/local/bucket/update >/dev/null 2>&1
 chmod +x /usr/local/bucket/update
