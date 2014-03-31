9ab340faf595940a0d17dcdff615cc482a614a07
Josh Trommel <josh@imjosh.me>
2014-03-30 20:34:58 -0700

    no more update.

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -11,12 +11,6 @@ cd ~
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile >/dev/null 2>&1
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc >/dev/null 2>&1
 
-cd /usr/local/bucket
-
-curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/update.sh >/dev/null 2>&1
-mv update.sh update
-chmod +x /usr/local/bucket/update
-
 cd $currentdir
 source ~/.bash_profile
 echo "dotfiles have been updated"
