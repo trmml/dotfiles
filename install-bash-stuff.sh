99d0951140bdc2cdfe72ac65e3fd8da6d81d23ec
Josh Trommel <josh@imjosh.me>
2014-03-31 23:10:56 -0700

    whhopps

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -11,6 +11,7 @@ cd ~
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile >/dev/null 2>&1
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc >/dev/null 2>&1
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.aliases >/dev/null 2>&1
+curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.profile >/dev/null 2>&1
 
 cd $currentdir
 source ~/.bash_profile
