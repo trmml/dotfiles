3c592e364bc7761b4d561be50840cbb6c300cf6d
Josh Trommel <joshception@icloud.com>
2014-04-01 21:39:37 -0700

    fix

--- install-bash-stuff.sh
+++ install-bash-stuff.sh
@@ -7,6 +7,8 @@ if [[ ! -d "/usr/local/bucket" ]]; then
 fi
 
 cd ~
+ echo '
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting' > ~/.profile
 
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bash_profile >/dev/null 2>&1
 curl -O https://raw.githubusercontent.com/trommel/dotfiles/master/.bashrc >/dev/null 2>&1
